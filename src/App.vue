f3c64280a5c2676127448ee4538b656554a72bd5
Daniel Vázquez <daniel.prolite@gmail.com>
2019-12-10 17:33:06 -0600

    Second section aspect fixed

--- src/App.vue
+++ src/App.vue
@@ -1,121 +1,212 @@
 <template>
-  <div id="app" class="container mx-auto">
+  <div id="app" class="mx-auto">
     <div class="container mx-auto">
-    <h2 class="font-sans font-extrabold text-4xl my-6">¿Qué hay en el menú?</h2>
-    <div class="flex content-around flex-wrap">
-      <div class="w-1/3 p-2">
-        <div class="bg-white">
-          <img class="object-cover w-full" src="../public/images/menu-tag-image.jpg">
+      <h2 class="font-sans font-extrabold text-4xl my-6">¿Qué hay en el menú?</h2>
+      <div class="flex content-around flex-wrap">
+        <div class="w-1/3 p-2">
+          <div class="bg-white">
+            <img class="object-cover w-full" src="../public/images/menu-tag-image.jpg" />
+          </div>Te ofrecemos los menús perfectos para tus eventos, comidas de equipo o reuniones en tu oficina.
+          <p>
+            <span class="block my-2 font-light text-greenSeaLight">
+              <a href="https://deliveroo.es/es/#top" target="_blank">
+                Ver catering para oficinas
+                <i class="fas fa-arrow-right"></i>
+              </a>
+            </span>
+          </p>
         </div>
-        Te ofrecemos los menús perfectos para tus eventos, comidas de equipo o reuniones en tu oficina.
-        <p><span class="block my-2 font-light text-greenSeaLight"><a href="https://deliveroo.es/es/#top" target="_blank">Ver catering para oficinas <i class="fas fa-arrow-right"></i></a></span></p>
-      </div>
-      <div class="w-2/3 p-2">
-        <div class="bg-white">
-          <img class="object-cover aspect-fill" src="../public/images/menu-tag-image-2.jpg">
+        <div class="w-2/3 p-2">
+          <div>
+            <img class="object-cover aspect-fill" src="../public/images/menu-tag-image-2.jpg" />
+          </div>
+          <div
+            class="pr-48"
+          >Una prueba de que puedes disfrutar de la mejor comida cada día a precios asequibles para tu bolsillo.</div>
+          <p>
+            <span class="block my-2 font-light text-greenSeaLight">
+              <a href="https://deliveroo.es/es/#top" target="_blank">
+                Ver sabrosos pero económicos
+                <i class="fas fa-arrow-right"></i>
+              </a>
+            </span>
+          </p>
         </div>
-        <div class="pr-48">Una prueba de que puedes disfrutar de la mejor comida cada día a precios asequibles para tu bolsillo.</div>
-        <p><span class="block my-2 font-light text-greenSeaLight"><a href="https://deliveroo.es/es/#top" target="_blank">Ver sabrosos pero económicos <i class="fas fa-arrow-right"></i></a></span></p>
-      </div>
-      <div class="w-2/2 p-2">
-        <div class="bg-white">
-          <img class="object-cover aspect-fill" src="../public/images/menu-tag-image-3.jpg">
+        <div class="w-2/2 p-2">
+          <div class="bg-white">
+            <img class="object-cover aspect-fill" src="../public/images/menu-tag-image-3.jpg" />
+          </div>Descubre los mejores restaurantes de la mano de nuestros expertos.
+          <p>
+            <span class="block my-2 font-light text-greenSeaLight">
+              <a href="https://deliveroo.es/es/#top" target="_blank">
+                Ver para compartir
+                <i class="fas fa-arrow-right"></i>
+              </a>
+            </span>
+          </p>
         </div>
-        Descubre los mejores restaurantes de la mano de nuestros expertos.
-        <p><span class="block my-2 font-light text-greenSeaLight"><a href="https://deliveroo.es/es/#top" target="_blank">Ver para compartir <i class="fas fa-arrow-right"></i></a></span></p>  
-      </div>
-      <div class="w-1/3 p-2">
-        <div class="bg-white">
-          <img class="object-cover aspect-fill" src="../public/images/menu-tag-image-4.jpg">
+        <div class="w-1/3 p-2">
+          <div class="bg-white">
+            <img class="object-cover aspect-fill" src="../public/images/menu-tag-image-4.jpg" />
+          </div>Platos deliciosos y nutritivos para recargar pilas.
+          <p>
+            <span class="block my-2 font-light text-greenSeaLight">
+              <a href="https://deliveroo.es/es/#top" target="_blank">
+                Ver recomendaciones locales
+                <i class="fas fa-arrow-right"></i>
+              </a>
+            </span>
+          </p>
         </div>
-        Platos deliciosos y nutritivos para recargar pilas.
-        <p><span class="block my-2 font-light text-greenSeaLight"><a href="https://deliveroo.es/es/#top" target="_blank">Ver recomendaciones locales <i class="fas fa-arrow-right"></i></a></span></p>
-      </div>
-      <div class="w-1/3 p-2">
-        <div class="bg-white">
-          <img class="object-cover aspect-fill" src="../public/images/menu-tag-image-5.jpg">
+        <div class="w-1/3 p-2">
+          <div class="bg-white">
+            <img class="object-cover aspect-fill" src="../public/images/menu-tag-image-5.jpg" />
+          </div>No sigas buscando, sólo los encontrarás aquí.
+          <p>
+            <span class="block my-2 font-light text-greenSeaLight">
+              <a href="https://deliveroo.es/es/#top" target="_blank">
+                Ver cocina saludable
+                <i class="fas fa-arrow-right"></i>
+              </a>
+            </span>
+          </p>
         </div>
-        No sigas buscando, sólo los encontrarás aquí.
-        <p><span class="block my-2 font-light text-greenSeaLight"><a href="https://deliveroo.es/es/#top" target="_blank">Ver cocina saludable <i class="fas fa-arrow-right"></i></a></span></p>  
-      </div>
-      <div class="w-2/3 p-2">
-        <div class="bg-white">
-          <img class="object-cover aspect-fill" src="../public/images/menu-tag-image-6.jpg">
+        <div class="w-2/3 p-2">
+          <div>
+            <img class="object-cover aspect-fill" src="../public/images/menu-tag-image-6.jpg" />
+          </div>Esos grandes clásicos a los que siempre vuelves porque nunca fallan.
+          <p>
+            <span class="block my-2 font-light text-greenSeaLight">
+              <a href="https://deliveroo.es/es/#top" target="_blank">
+                Ver exclusivo en delivery
+                <i class="fas fa-arrow-right"></i>
+              </a>
+            </span>
+          </p>
         </div>
-        Esos grandes clásicos a los que siempre vuelves porque nunca fallan.
-        <p><span class="block my-2 font-light text-greenSeaLight"><a href="https://deliveroo.es/es/#top" target="_blank">Ver exclusivo en delivery <i class="fas fa-arrow-right"></i></a></span></p>  
-      </div>
-      <div class="w-1/2 p-2">
-        <div class="bg-white">
-          <img class="object-cover aspect-fill" src="../public/images/menu-tag-image-7.jpg">
+        <div class="w-1/2 p-2">
+          <div class="bg-white">
+            <img class="object-cover aspect-fill" src="../public/images/menu-tag-image-7.jpg" />
+          </div>¿Necesitas una motivo para reunirte con tus amigos? Pedir comida es la excusa perfecta para compartir y disfrutar.
+          <p>
+            <span class="block my-2 font-light text-greenSeaLight">
+              <a href="https://deliveroo.es/es/#top" target="_blank">
+                Ver ver cocina tradicional
+                <i class="fas fa-arrow-right"></i>
+              </a>
+            </span>
+          </p>
         </div>
-        ¿Necesitas una motivo para reunirte con tus amigos? Pedir comida es la excusa perfecta para compartir y disfrutar.
-        <p><span class="block my-2 font-light text-greenSeaLight"><a href="https://deliveroo.es/es/#top" target="_blank">Ver ver cocina tradicional <i class="fas fa-arrow-right"></i></a></span></p>
       </div>
     </div>
 
-    </div>
-    
     <!-- Segunda Sección --->
-    <div class="container mx-auto bg-red-200">
-    <h2 class="font-sans font-extrabold text-4xl my-6">A ti te encanta, nosotros te lo llevamos</h2>
-    <div class="flex content-around flex-wrap">
-      <div class="w-1/3 p-2">
-        <div class="bg-white">
-          <img class="object-cover w-full" src="../public/images/menu-tag-image.jpg">
+    <div class="bg-orange-300">
+      <div class="container mx-auto">
+        <h2 class="font-sans font-extrabold text-4xl my-6">A ti te encanta, nosotros te lo llevamos</h2>
+        <div class="flex content-around flex-wrap font-plex">
+          <div class="w-1/3 p-2">
+            <div class="bg-white">
+              <img class="object-cover w-full" src="../public/images/menu-tag-image.jpg" />
+            </div>Te ofrecemos los menús perfectos para tus eventos, comidas de equipo o reuniones en tu oficina.
+            <p>
+              <span class="block my-2 font-light text-greenSeaLight">
+                <a href="https://deliveroo.es/es/#top" target="_blank">
+                  Ver catering para oficinas
+                  <i class="fas fa-arrow-right"></i>
+                </a>
+              </span>
+            </p>
+          </div>
+          <div class="w-2/3 p-2">
+            <div>
+              <img class="object-cover aspect-fill" src="../public/images/menu-tag-image-2.jpg" />
+            </div>
+            <div
+              class="pr-48"
+            >Una prueba de que puedes disfrutar de la mejor comida cada día a precios asequibles para tu bolsillo.</div>
+            <p>
+              <span class="block my-2 font-light text-greenSeaLight">
+                <a href="https://deliveroo.es/es/#top" target="_blank">
+                  Ver sabrosos pero económicos
+                  <i class="fas fa-arrow-right"></i>
+                </a>
+              </span>
+            </p>
+          </div>
+          <div class="w-2/2 p-2">
+            <div class="bg-white">
+              <img class="object-cover aspect-fill" src="../public/images/menu-tag-image-3.jpg" />
+            </div>Descubre los mejores restaurantes de la mano de nuestros expertos.
+            <p>
+              <span class="block my-2 font-light text-greenSeaLight">
+                <a href="https://deliveroo.es/es/#top" target="_blank">
+                  Ver para compartir
+                  <i class="fas fa-arrow-right"></i>
+                </a>
+              </span>
+            </p>
+          </div>
+          <div class="w-1/3 p-2">
+            <div class="bg-white">
+              <img class="object-cover aspect-fill" src="../public/images/menu-tag-image-4.jpg" />
+            </div>Platos deliciosos y nutritivos para recargar pilas.
+            <p>
+              <span class="block my-2 font-light text-greenSeaLight">
+                <a href="https://deliveroo.es/es/#top" target="_blank">
+                  Ver recomendaciones locales
+                  <i class="fas fa-arrow-right"></i>
+                </a>
+              </span>
+            </p>
+          </div>
+          <div class="w-1/3 p-2">
+            <div class="bg-white">
+              <img class="object-cover aspect-fill" src="../public/images/menu-tag-image-5.jpg" />
+            </div>No sigas buscando, sólo los encontrarás aquí.
+            <p>
+              <span class="block my-2 font-light text-greenSeaLight">
+                <a href="https://deliveroo.es/es/#top" target="_blank">
+                  Ver cocina saludable
+                  <i class="fas fa-arrow-right"></i>
+                </a>
+              </span>
+            </p>
+          </div>
+          <div class="w-2/3 p-2">
+            <div>
+              <img class="object-cover aspect-fill" src="../public/images/menu-tag-image-6.jpg" />
+            </div>Esos grandes clásicos a los que siempre vuelves porque nunca fallan.
+            <p>
+              <span class="block my-2 font-light text-greenSeaLight">
+                <a href="https://deliveroo.es/es/#top" target="_blank">
+                  Ver exclusivo en delivery
+                  <i class="fas fa-arrow-right"></i>
+                </a>
+              </span>
+            </p>
+          </div>
+          <div class="w-1/2 p-2">
+            <div class="bg-white">
+              <img class="object-cover aspect-fill" src="../public/images/menu-tag-image-7.jpg" />
+            </div>¿Necesitas una motivo para reunirte con tus amigos? Pedir comida es la excusa perfecta para compartir y disfrutar.
+            <p>
+              <span class="block my-2 font-light text-greenSeaLight">
+                <a href="https://deliveroo.es/es/#top" target="_blank">
+                  Ver ver cocina tradicional
+                  <i class="fas fa-arrow-right"></i>
+                </a>
+              </span>
+            </p>
+          </div>
         </div>
-        Te ofrecemos los menús perfectos para tus eventos, comidas de equipo o reuniones en tu oficina.
-        <p><span class="block my-2 font-light text-greenSeaLight"><a href="https://deliveroo.es/es/#top" target="_blank">Ver catering para oficinas <i class="fas fa-arrow-right"></i></a></span></p>
       </div>
-      <div class="w-2/3 p-2">
-        <div class="bg-white">
-          <img class="object-cover aspect-fill" src="../public/images/menu-tag-image-2.jpg">
-        </div>
-        <div class="pr-48">Una prueba de que puedes disfrutar de la mejor comida cada día a precios asequibles para tu bolsillo.</div>
-        <p><span class="block my-2 font-light text-greenSeaLight"><a href="https://deliveroo.es/es/#top" target="_blank">Ver sabrosos pero económicos <i class="fas fa-arrow-right"></i></a></span></p>
-      </div>
-      <div class="w-2/2 p-2">
-        <div class="bg-white">
-          <img class="object-cover aspect-fill" src="../public/images/menu-tag-image-3.jpg">
-        </div>
-        Descubre los mejores restaurantes de la mano de nuestros expertos.
-        <p><span class="block my-2 font-light text-greenSeaLight"><a href="https://deliveroo.es/es/#top" target="_blank">Ver para compartir <i class="fas fa-arrow-right"></i></a></span></p>  
-      </div>
-      <div class="w-1/3 p-2">
-        <div class="bg-white">
-          <img class="object-cover aspect-fill" src="../public/images/menu-tag-image-4.jpg">
-        </div>
-        Platos deliciosos y nutritivos para recargar pilas.
-        <p><span class="block my-2 font-light text-greenSeaLight"><a href="https://deliveroo.es/es/#top" target="_blank">Ver recomendaciones locales <i class="fas fa-arrow-right"></i></a></span></p>
-      </div>
-      <div class="w-1/3 p-2">
-        <div class="bg-white">
-          <img class="object-cover aspect-fill" src="../public/images/menu-tag-image-5.jpg">
-        </div>
-        No sigas buscando, sólo los encontrarás aquí.
-        <p><span class="block my-2 font-light text-greenSeaLight"><a href="https://deliveroo.es/es/#top" target="_blank">Ver cocina saludable <i class="fas fa-arrow-right"></i></a></span></p>  
-      </div>
-      <div class="w-2/3 p-2">
-        <div class="bg-white">
-          <img class="object-cover aspect-fill" src="../public/images/menu-tag-image-6.jpg">
-        </div>
-        Esos grandes clásicos a los que siempre vuelves porque nunca fallan.
-        <p><span class="block my-2 font-light text-greenSeaLight"><a href="https://deliveroo.es/es/#top" target="_blank">Ver exclusivo en delivery <i class="fas fa-arrow-right"></i></a></span></p>  
-      </div>
-      <div class="w-1/2 p-2">
-        <div class="bg-white">
-          <img class="object-cover aspect-fill" src="../public/images/menu-tag-image-7.jpg">
-        </div>
-        ¿Necesitas una motivo para reunirte con tus amigos? Pedir comida es la excusa perfecta para compartir y disfrutar.
-        <p><span class="block my-2 font-light text-greenSeaLight"><a href="https://deliveroo.es/es/#top" target="_blank">Ver ver cocina tradicional <i class="fas fa-arrow-right"></i></a></span></p>
-      </div>
-    </div>
     </div>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'app',
-}
+  name: "app"
+};
 </script>
